f5c131e39810c543daaed624f3a4ed941e78b05b
SKeoughCode <131558651+SKeoughCode@users.noreply.github.com>
2023-05-04 16:08:33 -0400

    Final Commits

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM Loaded");
+    let temp = document.getElementById('text');
+    temp.textContent = "This is really cool!";
+});
+
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
